2c475971bf4d016125198263ccefa724c6263b3c
Arthur Corona <coronaggp@gmail.com>
2023-02-23 19:50:04 -0300

    validated button

--- build/js/firebase.js
+++ build/js/firebase.js
@@ -1,6 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// Import the functions you need from the SDKs you need;
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js";
+  import { getFirestore} from "https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,9 +19,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-  const db = firebase.firestore() // database fireabse
-  const auth = firebase.auth() //authenticator firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app); 
+  const db = getFirestore(app);// database fireabse
   const storage = firebase.storage() 
   const ref = storage.ref("/folder do firebase") // referencia para a pasta raiz do storage no firebase, da pra especificar ainda mais o arquivo preenchendo o ()
--- build/js/script.js
+++ build/js/script.js
@@ -4,15 +4,29 @@ const form = {
     day: () => form.date().getDate(),
     month: () => form.date().getMonth() + 1,
     year: () => form.date().getFullYear(),
-    currentDate: () => `${form.day()}/${form.month()}/${form.year()}`
+    currentDate: () => `${form.day()}/${form.month()}/${form.year()}`,
+
+    clientInput: () => document.getElementsByClassName("client-input").value,
+    volumesInput: () => document.getElementsByClassName("vol-input").value,
+    submitButton: () => document.getElementById("submit-button"),
+    allInputs: () => document.querySelectorAll("input"),
+
+
+
 }
 
-// console.log(form.currentDate()); // today!
+const items = 
+    {
+        client: "name",
+        volumes: 0,
+        date: new Date().toLocaleDateString()
+    }
 
 document.addEventListener("DOMContentLoaded", () => {
     getCurrentDate()
 })
 
+// put the date in HTML
 function getCurrentDate() {
     document.querySelector(".date-today").innerHTML += 
         `
@@ -21,3 +35,52 @@ function getCurrentDate() {
         </div>
         `
 }
+
+//create the order in database
+function createOrderInDB() {
+    db.collection('withdraw').doc().update({
+        client: form.clientInput(),
+        vol: form.volumesInput()
+      }).then(() => {
+        document.location.reload(true)
+      }).catch(error => {
+        console.log(error);
+    })
+}
+
+//validate inputs/button
+
+let inputs = form.allInputs()
+let submitButton = form.submitButton()
+
+function checkInputs(inputs) {
+
+  let filled = true;
+
+  inputs.forEach(function(input) {
+
+    if(input.value === "") {
+        filled = false;
+    }
+
+  })
+  return filled;
+}
+
+inputs.forEach(function(input) {
+  
+  input.addEventListener("keyup", function() {
+
+    if(checkInputs(inputs)) {
+      submitButton.disabled = false;
+    }
+    else {
+      submitButton.disabled = true;
+    }
+  })
+})
+
+function writeOrderInHTML() {
+    
+}  
+
